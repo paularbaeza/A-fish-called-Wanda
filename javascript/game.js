b914b9e39865f789fc0b35ba312dbc931e2abff2
Paula Rodriguez Baeza <paularb923@hotmail.com>
2022-07-11 12:24:24 +0100

    game over when 0 lifes

--- javascript/game.js
+++ javascript/game.js
@@ -86,6 +86,9 @@ class Game {
         this.wanda.canCollide = false;
         this.wanda.faceSickWanda();
         setTimeout(this.wanda.afterWandaLoseLife, 2000);
+        if (lifesDOM.innerText==="0"){
+          this.gameOver()
+        }
       }
 
       if (
@@ -111,6 +114,9 @@ class Game {
         this.wanda.canCollide = false;
         this.wanda.faceSickWanda();
         setTimeout(this.wanda.afterWandaLoseLife, 2000);
+        if (lifesDOM.innerText==="0"){
+          this.gameOver()
+        }
       }
 
       if (
@@ -136,6 +142,9 @@ class Game {
         this.wanda.canCollide = false;
         this.wanda.faceSickWanda();
         setTimeout(this.wanda.afterWandaLoseLife, 2000);
+        if (lifesDOM.innerText==="0"){
+          this.gameOver()
+        }
       }
 
       if (
