dcc7955c0e5a1377c1c473fb2c378b37f5600d92
Paula Rodriguez Baeza <paularb923@hotmail.com>
2022-07-14 21:13:11 +0100

    added loop to main song

--- javascript/game.js
+++ javascript/game.js
@@ -16,6 +16,7 @@ class Game {
     this.showFlash = false;
     this.gameMusic = new Audio("./sounds/bajo-del-mar.mp3");
     this.gameMusic.volume = 0.2;
+    this.gameMusic.loop = true;
     this.winLife = new Audio("./sounds/woo-hoo.mp3");
     this.winLife.volume = 0.2;
     this.loseLife = new Audio("./sounds/pierde-vida.mp3");
